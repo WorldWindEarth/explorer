fbfc57d4c48c56424b1011e70b5f29074ef2e646
Bruce Schubert <bruce@emxsys.com>
2016-08-01 05:09:54 -0700

    Fixed typo's in member names in ContextSensitive.

--- root/js/model/util/ContextSensitive.js
+++ root/js/model/util/ContextSensitive.js
@@ -8,7 +8,7 @@
 /**
  * ContextSenstive is a mix-in module that adds the "Contextual Menu" capabilities to an object.
  * @param {Publisher} publisher Extends the object by adding the event generator
- * @returns {ContextSenstive}
+ * @returns {ContextSensitive}
  * 
  * @author Bruce Schubert
  */
@@ -18,9 +18,9 @@ define([
     function (ko, publisher) {
         "use strict";
         
-        var ContextSenstive = {
+        var ContextSensitive = {
             showContextMenu: function (params) {
-                if (this.isContextSenstive()) {
+                if (this.isContextSensitive()) {
                     this.showMyContextMenu(params);
                 }
             },
@@ -29,30 +29,30 @@ define([
              * @param {Object} o The object that will become contextsenstive.
              * @param {Boolean Function()} openCallback The function that performs the edit.
              */
-            makeContextSenstive: function (o, contextMenuCallback) {
+            makeContextSensitive: function (o, contextMenuCallback) {
                 // Ensure we don't duplicate 
                 if (o.showContextMenu) {
                     return; // o is already context senstive
                 }
                 // Add the function(s)
                 var i;
-                for (i in ContextSenstive) {
-                    if (ContextSenstive.hasOwnProperty(i) && typeof ContextSenstive[i] === 'function') {
-                        if (ContextSenstive[i] === this.makeContextSenstive) {
+                for (i in ContextSensitive) {
+                    if (ContextSensitive.hasOwnProperty(i) && typeof ContextSensitive[i] === 'function') {
+                        if (ContextSensitive[i] === this.makeContextSensitive) {
                             continue;
                         }
-                        o[i] = ContextSenstive[i];
+                        o[i] = ContextSensitive[i];
                     }
                 }
                 // Add the properties
-                o.isContextSenstive = ko.observable(true);
+                o.isContextSensitive = ko.observable(true);
                 o.showMyContextMenu = contextMenuCallback;
                 
                 // Add the Publisher capability so that events can be generated.
                 publisher.makePublisher(o);
             }
         };
-        return ContextSenstive;
+        return ContextSensitive;
     }
 );
 
