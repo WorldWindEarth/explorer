f846a88ee4799fdc82c88a03b3fd9864bf7f4001
Bruce Schubert <bruce@emxsys.com>
2017-07-16 06:25:25 -0700

    Removed stale code.

--- root/js/model/Explorer.js
+++ root/js/model/Explorer.js
@@ -225,36 +225,6 @@ define(['jquery',
                 this.mousePoint = null;
             }
         };
-        /**
-         * Holds configuration parameters for WWE. Applications may modify these parameters prior to creating
-         * their first Explorer objects. Configuration properties are:
-         * <ul>
-         *     <li><code>startupLatitude</code>: Initial "look at" latitude. Default is Ventura, CA.
-         *     <li><code>startupLongitude</code>: Initial "look at" longitude. Default is Venura, CA.
-         *     <li><code>startupLongitude</code>: Initial altitude/eye position. Default 0.
-         *     <li><code>startupHeading</code>: Initial view heading. Default 0.
-         *     <li><code>startupTilt</code>: Initial view tilt. Default 0.
-         *     <li><code>startupRoll</code>: Initial view roll. Default 0.
-         *     <li><code>viewControlOrientation</code>: horizontal or vertical. Default vertical.
-         *     <li><code>showPanControl</code>: Show pan (left/right/up/down) controls. Default false.
-         *     <li><code>showExaggerationControl</code>: Show vertical exaggeration controls. Default false.
-         * </ul>
-         */
-        Explorer.configuration = {
-            imageryDetailHint: (window.screen.width < 768 ? -0.1 : (window.screen.width < 1024 ? 0.0 : (window.screen.width < 1280 ? 0.1 : 0.2))),
-            markerLabels: constants.MARKER_LABEL_NAME,
-            startupLatitude: 34.29,
-            startupLongitude: -119.29,
-            startupAltitude: 1000000,
-            startupHeading: 0,
-            startupTilt: 0,
-            startupRoll: 0,
-            showPanControl: true,
-            showExaggerationControl: true,
-            showFieldOfViewControl: false,
-            terrainSampleRadius: 30,
-            viewControlOrientation: "vertical"
-        };
 
         return Explorer;
     }
