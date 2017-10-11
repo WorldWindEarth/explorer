7d1ce5ec3afd49b77ddefa601ff2e66b5cfd7e34
Bruce Schubert <bruce@emxsys.com>
2017-10-11 07:56:21 -0700

    Added additional functionallity to the LayersViewModel. - Layer sorting - Zoom to layer - Fixed add/remove server layers

--- root/js/views/LayersViewModel.js
+++ root/js/views/LayersViewModel.js
@@ -11,8 +11,9 @@
  * @param {type} $
  * @returns {LayersViewModel}
  */
-define(['knockout', 'jquery', 'jqueryui', 'bootstrap', 'model/Constants'],
-        function (ko, $, jqueryui, boostrap, constants) {
+define(['knockout', 'jquery', 'jqueryui', 'bootstrap', 'model/Constants', 'model/util/Log',
+],
+        function (ko, $, jqueryui, boostrap, constants, log) {
 
             /**
              * The view model for the Layers panel.
@@ -33,12 +34,15 @@ define(['knockout', 'jquery', 'jqueryui', 'bootstrap', 'model/Constants'],
                 // Layer type options
                 self.optionValues = ["WMS Layer", "WMTS Layer", "KML file", "Shapefile"];
                 self.selectedOptionValue = ko.observable(self.optionValues[0]);
-                
+
                 /**
                  * An observable array of servers
                  */
                 this.servers = layerManager.servers;
-                self.serverAddress = ko.observable("http://neowms.sci.gsfc.nasa.gov/wms/wms");
+                // Setting a default server address to some interesting data
+                self.serverAddress = ko.observable("http://neowms.sci.gsfc.nasa.gov/wms/wms"); // CORS is disabled now in here
+                // self.serverAddress = ko.observable("https://worldwind25.arc.nasa.gov/wms");
+
 
                 /**
                  * Toggles the selected layer's visibility on/off
@@ -55,7 +59,7 @@ define(['knockout', 'jquery', 'jqueryui', 'bootstrap', 'model/Constants'],
                  * @param {Object} layer The selected layer in the layer collection
                  */
                 self.onEditSettings = function (layer) {
-                    
+
                     $('#opacity-slider').slider({
                         animate: 'fast',
                         min: 0,
@@ -67,45 +71,69 @@ define(['knockout', 'jquery', 'jqueryui', 'bootstrap', 'model/Constants'],
                         },
                         step: 0.1
                     });
-                    
+
                     $("#layer-settings-dialog").dialog({
                         autoOpen: false,
                         title: layer.name()
                     });
-                    
+
                     //console.log(layer.name() + ":  " + layer.opacity());
                     $("#opacity-slider").slider("option", "value", layer.opacity());
                     $("#layer-settings-dialog").dialog("open");
                 };
-                
-                
+
+
                 /**
                  * Opens the Add Layer dialog.
                  */
-                self.onAddLayer = function() {
+                self.onAddLayer = function () {
                     $("#add-layer-dialog").dialog({
                         autoOpen: false,
                         title: "Add Layer"
                     });
-                    
+
                     $("#add-layer-dialog").dialog("open");
                 };
-                
-                
-                self.onAddServer  = function() {
-                    layerManager.addServer(self.serverAddress());
+
+
+                self.onAddServer = function () {
+                    layerManager.addWmsServer(self.serverAddress());
                     return true;
                 };
+
+                self.onZoomToLayer = function (layer){
+                    layerManager.zoomToLayer(layer);
+                };
+
+                self.onMoveLayerUp = function (layer) {
+                    layerManager.moveLayer(layer, "up");
+                };
+
+                self.onMoveLayerToTop = function(layer) {
+                    layerManager.moveLayer(layer, 0);
+                };
+
+                self.onMoveLayerDown = function (layer) {
+                    layerManager.moveLayer(layer, "down");
+                }
 
                 /**
                  * Add the supplied layer from the server's capabilities to the active layers
                  */
-                this.onServerLayerClicked = function(layerNode, event){
+                this.onServerLayerClicked = function (layerNode, event) {
+                    var layer;
                     if (!layerNode.isChecked()) {
                         // TODO: Open dialog to select a layer category
-                        layerManager.addLayerFromCapabilities(layerNode.layerCaps, constants.LAYER_CATEGORY_OVERLAY);
+                        layerManager.addLayerFromCapabilities(layerNode.layerCaps, constants.LAYER_CATEGORY_BASE);
                     } else {
-                        layerManager.removeLayer(layerNode.layerCaps);
+                        // Find the first layer with a displayName matching the title
+                        layer = layerManager.findLayer(layerNode.layerCaps.title);
+                        if (layer) {
+                            layerManager.removeLayer(layer);
+                        } else {
+                            log.error("LayersViewModel", "onServerLayerClicked",
+                                    "Could not find a layer to removed named " + layerNode.layerCaps.title);
+                        }
                     }
                     return true;
                 };
