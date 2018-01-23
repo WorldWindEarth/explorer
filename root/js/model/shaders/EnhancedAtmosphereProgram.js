e69b2771f5d02e37bd04f3a323f63627c9eaff72
Bruce Schubert <bruce@emxsys.com>
2018-01-23 08:48:10 -0800

    Refactored EnhancedAtmosphereLayer to inherit from WorldWind.AtmosphereProgram - Adds the opacity uniform. - Re: issue #71

--- root/js/model/shaders/EnhancedAtmosphereProgram.js
+++ root/js/model/shaders/EnhancedAtmosphereProgram.js
@@ -6,6 +6,9 @@
 /* global WorldWind */
 
 /**
+ * This module is a copy of the WorldWind.AtmosphereProgram, modified to add support for transparency.
+ * Search for occurancies of "opacity" to locate the addtions/modifications.
+ * 
  * @exports EnhancedAtmosphereProgram
  */
 define(['worldwind'],
@@ -21,7 +24,7 @@ define(['worldwind'],
          *
          * @alias EnhancedAtmosphereProgram
          * @constructor
-         * @augments GpuProgram
+         * @augments AtmosphereProgram
          * @classdesc EnhancedAtmosphereProgram is a GLSL program that draws the atmosphere.
          * @param {WebGLRenderingContext} gl The current WebGL context.
          * @throws {ArgumentError} If the shaders cannot be compiled, or linking of
@@ -30,112 +33,7 @@ define(['worldwind'],
         var EnhancedAtmosphereProgram = function (gl, vertexShaderSource, fragmentShaderSource, attribute) {
 
             // Call to the superclass, which performs shader program compiling and linking.
-            WorldWind.GpuProgram.call(this, gl, vertexShaderSource, fragmentShaderSource, attribute);
-
-
-            // Frag color mode indicates the atmospheric scattering color components written to the fragment color.
-            this.FRAGMODE_SKY = 1;
-            this.FRAGMODE_GROUND_PRIMARY = 2;
-            this.FRAGMODE_GROUND_SECONDARY = 3;
-            this.FRAGMODE_GROUND_PRIMARY_TEX_BLEND = 4;
-
-            /**
-             * The globe's atmosphere altitude.
-             * @type {Number}
-             * @default 160000.0 meters
-             */
-            this.altitude = 160000;
-
-            /**
-             * This atmosphere's Rayleigh scale depth.
-             * @type {Number}
-             * @default 0.25
-             */
-            this.rayleighScaleDepth = 0.25;
-
-            /**
-             * The WebGL location for this program's 'fragMode' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.fragModeLocation = this.uniformLocation(gl, "fragMode");
-
-            /**
-             * The WebGL location for this program's 'mvpMatrix' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.mvpMatrixLocation = this.uniformLocation(gl, "mvpMatrix");
-
-            /**
-             * The WebGL location for this program's 'texCoordMatrix' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.texCoordMatrixLocation = this.uniformLocation(gl, "texCoordMatrix");
-
-            /**
-             * The WebGL location for this program's 'vertexOrigin' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.vertexOriginLocation = this.uniformLocation(gl, "vertexOrigin");
-
-            /**
-             * The WebGL location for this program's 'eyePoint' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.eyePointLocation = this.uniformLocation(gl, "eyePoint");
-
-            /**
-             * The WebGL location for this program's 'eyeMagnitude' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.eyeMagnitudeLocation = this.uniformLocation(gl, "eyeMagnitude");
-
-            /**
-             * The WebGL location for this program's 'eyeMagnitude2' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.eyeMagnitude2Location = this.uniformLocation(gl, "eyeMagnitude2");
-
-            /**
-             * The WebGL location for this program's 'lightDirection' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.lightDirectionLocation = this.uniformLocation(gl, "lightDirection");
-
-            /**
-             * The WebGL location for this program's 'atmosphereRadius' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.atmosphereRadiusLocation = this.uniformLocation(gl, "atmosphereRadius");
-
-            /**
-             * The WebGL location for this program's 'atmosphereRadius2' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.atmosphereRadius2Location = this.uniformLocation(gl, "atmosphereRadius2");
-
-            /**
-             * The WebGL location for this program's 'globeRadius' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.globeRadiusLocation = this.uniformLocation(gl, "globeRadius");
-
-            /**
-             * The WebGL location for this program's 'scale' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.scaleLocation = this.uniformLocation(gl, "scale");
+            WorldWind.AtmosphereProgram.call(this, gl, vertexShaderSource, fragmentShaderSource, attribute);
 
             /**
              * The WebGL location for this program's 'opacity' uniform.
@@ -144,21 +42,6 @@ define(['worldwind'],
              */
             this.opacityLocation = this.uniformLocation(gl, "opacity");
 
-            /**
-             * The WebGL location for this program's 'scaleDepth' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.scaleDepthLocation = this.uniformLocation(gl, "scaleDepth");
-
-            /**
-             * The WebGL location for this program's 'scaleOverScaleDepth' uniform.
-             * @type {WebGLUniformLocation}
-             * @readonly
-             */
-            this.scaleOverScaleDepthLocation = this.uniformLocation(gl, "scaleOverScaleDepth");
-
-            this.scratchArray9 = new Float32Array(9);
         };
 
         /**
@@ -168,151 +51,8 @@ define(['worldwind'],
          */
         EnhancedAtmosphereProgram.key = "EnhancedGpuAtmosphereProgram";
 
-        // Inherit from GpuProgram.
-        EnhancedAtmosphereProgram.prototype = Object.create(WorldWind.GpuProgram.prototype);
-
-        /**
-         * Returns the atmosphere's altitude.
-         * @returns {Number} The atmosphere's altitude in meters.
-         */
-        EnhancedAtmosphereProgram.prototype.getAltitude = function () {
-            return this.altitude;
-        };
-
-        /**
-         * Loads the specified number as the value of this program's 'fragMode' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Number} fragMode The frag mode value.
-         * @throws {ArgumentError} If the specified number is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadFragMode = function (gl, fragMode) {
-            if (!fragMode) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadFragMode", "missingFragMode"));
-            }
-
-            gl.uniform1i(this.fragModeLocation, fragMode);
-        };
-
-        /**
-         * Loads the specified matrix as the value of this program's 'mvpMatrix' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Matrix} matrix The matrix to load.
-         * @throws {ArgumentError} If the specified matrix is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadModelviewProjection = function (gl, matrix) {
-            if (!matrix) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadModelviewProjection",
-                        "missingMatrix"));
-            }
-
-            this.loadUniformMatrix(gl, matrix, this.mvpMatrixLocation);
-        };
-
-        /**
-         * Loads the specified vector as the value of this program's 'vertexOrigin' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Vec3} vector The vector to load.
-         * @throws {ArgumentError} If the specified vector is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadVertexOrigin = function (gl, vector) {
-            if (!vector) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadVertexOrigin", "missingVector"));
-            }
-
-            gl.uniform3f(this.vertexOriginLocation, vector[0], vector[1], vector[2]);
-        };
-
-        /**
-         * Loads the specified vector as the value of this program's 'lightDirection' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Vec3} vector The vector to load.
-         * @throws {ArgumentError} If the specified vector is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadLightDirection = function (gl, vector) {
-            if (!vector) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadLightDirection", "missingVector"));
-            }
-
-            gl.uniform3f(this.lightDirectionLocation, vector[0], vector[1], vector[2]);
-        };
-
-        /**
-         * Loads the specified vector as the value of this program's 'lightDirection' uniform variable,
-         * the magnitude's specified vector as the value of this program's 'eyeMagnitude' uniform variable and
-         * the squared magnitude's specified vector as the value of this program's 'eyeMagnitude2' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Vec3} vector The vector to load.
-         * @throws {ArgumentError} If the specified vector is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadEyePoint = function (gl, vector) {
-            if (!vector) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadEyePoint", "missingVector"));
-            }
-
-            gl.uniform3f(this.eyePointLocation, vector[0], vector[1], vector[2]);
-            gl.uniform1f(this.eyeMagnitudeLocation, vector.magnitude());
-            gl.uniform1f(this.eyeMagnitude2Location, vector.magnitudeSquared());
-        };
-
-        /**
-         * Loads the specified number as the value of this program's 'globeRadius' uniform variable and the specified
-         * number which add the altitude value as the value of this program's 'atmosphereRadius' uniform variable.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Number} globeRadius The globe radius value.
-         * @throws {ArgumentError} If the specified number is null or undefined.
-         */
-        EnhancedAtmosphereProgram.prototype.loadGlobeRadius = function (gl, globeRadius) {
-            if (!globeRadius) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadGlobeRadius",
-                        "missingGlobeRadius"));
-            }
-
-            var gr = globeRadius;
-            var ar = gr + this.altitude;
-
-            gl.uniform1f(this.globeRadiusLocation, gr);
-            gl.uniform1f(this.atmosphereRadiusLocation, ar);
-            gl.uniform1f(this.atmosphereRadius2Location, ar * ar);
-        };
-
-        /**
-         * Sets the program's 'scale', 'scaleDepth' and 'scaleOverScaleDepth' uniform variables.
-         *
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         */
-        EnhancedAtmosphereProgram.prototype.setScale = function (gl) {
-            gl.uniform1f(this.scaleLocation, 1 / this.getAltitude());
-            gl.uniform1f(this.scaleDepthLocation, this.rayleighScaleDepth);
-            gl.uniform1f(this.scaleOverScaleDepthLocation, (1 / this.getAltitude()) / this.rayleighScaleDepth);
-        };
-
-        /**
-         * Loads the specified matrix as the value of this program's 'texCoordMatrix' uniform variable.
-         * @param {WebGLRenderingContext} gl The current WebGL context.
-         * @param {Matrix3} matrix The texture coordinate matrix.
-         */
-        EnhancedAtmosphereProgram.prototype.loadTexMatrix = function (gl, matrix) {
-            if (!matrix) {
-                throw new WorldWind.ArgumentError(
-                    WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "EnhancedAtmosphereProgram", "loadTexMatrix",
-                        "missingMatrix"));
-            }
-
-            matrix.columnMajorComponents(this.scratchArray9);
-            gl.uniformMatrix3fv(this.texCoordMatrixLocation, false, this.scratchArray9);
-        };
+        // Inherit from AtmoshpereProgram.
+        EnhancedAtmosphereProgram.prototype = Object.create(WorldWind.AtmosphereProgram.prototype);
 
         /**
          * Loads the specified opacity as the value of this program's 'opacity' uniform variable.
