44520ff0bb395c334d07a462efb372ed64228481
Bruce Schubert <bruce@emxsys.com>
2018-02-11 06:47:49 -0800

    Added support for slow connections. - Added a RequireJS error handler to provide feedback/advice on timeout errors. - Increased the timeout from 7s to 15s to handle slow 3D connections.

--- root/js/main.js
+++ root/js/main.js
@@ -19,19 +19,37 @@ window.DEBUG = false;
 requirejs.config({
     // Path mappings for the logical module names
     paths: {
+        // Bootstrap responsive layout
         'bootstrap': window.DEBUG ? 'libs/bootstrap/v3.3.6/bootstrap.min' : 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min',
+        // d3 graphics library
         'd3': window.DEBUG ? 'libs/d3/d3' : 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.min',
+        // RequireJS plugin to wait for DOM ready
+        'domReady': 'libs/require/domReady',
+        // Dragula drag-n-drop library
         'dragula': 'libs/dragula/dragula',
+        // Knockout Model-View-View Model
         'knockout': window.DEBUG ? 'libs/knockout/knockout-3.4.0.debug' : 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+        // RequireJS plugin to load 'i18n!' prefixed modules
+        'i18n': 'libs/require/i18n',
+        // The ubuiqutious JQuery library
         'jquery': window.DEBUG ? 'libs/jquery/jquery-2.1.3' : 'http://code.jquery.com/jquery-2.2.4.min',
+        // JQuery UI elements
         'jqueryui': window.DEBUG ? 'libs/jquery-ui/jquery-ui-1.11.4' : 'http://code.jquery.com/ui/1.12.1/jquery-ui.min',
+        // JQuery UI based 'growl' messaging
         'jquery-growl': 'libs/jquery-plugins/jquery.growl',
+        // Moment date/time library
         'moment': window.DEBUG ? 'libs/moment/moment-2.14.1.min' : 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min',
+        // URL search param parsing
         'url-search-params': 'libs/url-search-params/url-search-params.max.amd',
+        // RequireJS plugin to load text/html files using the 'text!' prefixed modules
+        'text': 'libs/require/text',
+        // VisJS charting library
         'vis': window.DEBUG ? 'libs/vis/v4.16.1/vis' : 'https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min',
-        'worldwind': window.DEBUG ? 'libs/webworldwind/v0.9.0/worldwind' : 'https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/worldwind.min',
-        'model': 'model' // root application path
+        // NASA WorldWind
+        'worldwind': window.DEBUG ? 'libs/webworldwind/v0.9.0/worldwind' : 'https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/worldwind.min'
     },
+    // Increase the time to wait before giving up on loading a script to avoid timeout erros on slow 3G connections (default 7s)
+    waitSeconds: 15,
     // Shim configuration for Bootstrap's JQuery dependency
     shim: {
         "bootstrap": {
@@ -42,39 +60,58 @@ requirejs.config({
 });
 
 /**
+ * Override the RequireJS error handling to provide some user feedback.
+ * @param {Object} err
+ */
+requirejs.onError = function (err) {
+    if (err.requireType === 'timeout') {
+        alert("A timeout occurred while loading scripts.\n\
+The server may be busy or you have a slow connection.\n\
+Try refreshing the page or try again later.\n\n" + err);
+    } else {
+        throw err;
+    }
+};
+
+/**
  * The application's main entry point, called in index.html
  * 
- * @param {Knockout} ko
- * @param {JQuery} $
  * @param {Config} config Explorer configuration object
  * @param {Constants} constants Explorer constants
+ * @param {Knockout} ko
+ * @param {JQuery} $
+ * @param {domReady} domReady function called once the DOM is ready
  */
 require([
-    'knockout',
-    'jquery',
     'model/Config',
     'model/Constants',
+    'knockout',
+    'jquery',
+    'domReady',
     'jqueryui',
-    'worldwind'], function (ko, $, config, constants) { // this callback gets executed after all modules defined in the array are loaded
-    
+    'worldwind'], function (config, constants, ko, $, domReady) { // this callback gets executed after all modules defined in the array are loaded
+
     "use strict";
 
-    // -----------------------------------------------------------
-    // Add handlers for UI elements
-    // -----------------------------------------------------------
-    // Auto-collapse navbar when its tab items are clicked
-    $('.navbar-collapse a[role="tab"]').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
-    // Auto-scroll-into-view expanded dropdown menus
-    $('.dropdown').on('shown.bs.dropdown', function (event) {
-        event.target.scrollIntoView(false); // align to bottom
-    });
-    // Auto-expand menu section-bodies when not small
-    $(window).resize(function () {
-        if ($(window).width() >= 768) {
-            $('.section-body').collapse('show');
-        }
+    // This function is called once the DOM is ready.
+    domReady(function () {
+        // -----------------------------------------------------------
+        // Add handlers for UI elements
+        // -----------------------------------------------------------
+        // Auto-collapse navbar when its tab items are clicked
+        $('.navbar-collapse a[role="tab"]').click(function () {
+            $('.navbar-collapse').collapse('hide');
+        });
+        // Auto-scroll-into-view expanded dropdown menus
+        $('.dropdown').on('shown.bs.dropdown', function (event) {
+            event.target.scrollIntoView(false); // align to bottom
+        });
+        // Auto-expand menu section-bodies when not small
+        $(window).resize(function () {
+            if ($(window).width() >= 768) {
+                $('.section-body').collapse('show');
+            }
+        });
     });
 
     // --------------------------------------------------------
