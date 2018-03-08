924f01728f40d15b3299bbf85cb240504fe1f24b
Bruce Schubert <bruce@emxsys.com>
2018-03-08 08:50:25 -0800

    Added Web WorldWind v0.9.0 memory management performance fixes.

--- root/js/main.js
+++ root/js/main.js
@@ -86,13 +86,14 @@ Try refreshing the page or try again later.\n\n" + err);
  * @param {JQuery} $
  */
 require([
+    'WorldWindFixes',
     'model/Config',
     'model/Constants',
     'pace',
     'jquery',
     'domReady!', // The value for domReady! is the current document
     'worldwind'],
-    function (config, constants, pace, $) {
+    function (fixes, config, constants, pace, $) {
         "use strict";
 
         // Start a  progress counter
@@ -125,6 +126,7 @@ require([
         // ----------------
         // Setup WorldWind
         // ----------------
+        fixes.applyLibraryFixes();
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
         if (window.DEBUG) {
             // Use local resources
@@ -136,6 +138,8 @@ require([
         
         // Initialize the WorldWindow virtual globe with the specified HTML5 canvas
         var wwd = new WorldWind.WorldWindow("globe-canvas");
+        fixes.applyWorldWindowFixes(wwd);
+        
         // Provide an initial location to view
         wwd.navigator.lookAtLocation.latitude = config.startupLatitude;
         wwd.navigator.lookAtLocation.longitude = config.startupLongitude;
