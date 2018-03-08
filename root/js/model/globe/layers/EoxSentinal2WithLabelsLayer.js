d3ed704ba713b5550aebe95e6470c11f997056bc
Bruce Schubert <bruce@emxsys.com>
2018-03-08 08:54:23 -0800

    Changed layers to use EnhancedWmsLayer.

--- root/js/model/globe/layers/EoxSentinal2WithLabelsLayer.js
+++ root/js/model/globe/layers/EoxSentinal2WithLabelsLayer.js
@@ -15,45 +15,48 @@
  * 
  * @returns {EoxSentinal2WithLabelsLayer}
  */
-define(['worldwind'], function () {
-    "use strict";
-    /**
-     * Constructs an imagery layer from Copernicus Sentinel data with overlay from OpenSteetMap.
-     * @constructor
-     * @augments WmsLayer
-     */
-    var EoxSentinal2WithLabelsLayer = function () {
-        var cfg = {
-            title: "Sentinel-2 cloudless with labels by EOX",
-            version: "1.3.0",
-            service: "https://tiles.maps.eox.at/wms",
-            layerNames: "s2cloudless,overlay",
-            sector: new WorldWind.Sector(-90.0, 90.0, -180, 180),
-            levelZeroDelta: new WorldWind.Location(36, 36),
-            numLevels: 16,
-            format: "image/png",
-            size: 256,
-            coordinateSystem: "EPSG:4326", // optional
-            styleNames: "" // (optional): {String} A comma separated list of the styles to include in this layer.</li>
+define([
+    'model/globe/layers/EnhancedWmsLayer',
+    'worldwind'],
+    function (EnhancedWmsLayer) {
+        "use strict";
+        /**
+         * Constructs an imagery layer from Copernicus Sentinel data with overlay from OpenSteetMap.
+         * @constructor
+         * @augments WmsLayer
+         */
+        var EoxSentinal2WithLabelsLayer = function () {
+            var cfg = {
+                title: "Sentinel-2 cloudless with labels by EOX",
+                version: "1.3.0",
+                service: "https://tiles.maps.eox.at/wms",
+                layerNames: "s2cloudless,overlay",
+                sector: new WorldWind.Sector(-90.0, 90.0, -180, 180),
+                levelZeroDelta: new WorldWind.Location(180, 180),
+                numLevels: 16,
+                format: "image/png",
+                size: 256,
+                coordinateSystem: "EPSG:4326", // optional
+                styleNames: "" // (optional): {String} A comma separated list of the styles to include in this layer.</li>
+            };
+
+            EnhancedWmsLayer.call(this, cfg);
+
+            // Make this layer opaque
+            this.opacity = 1.0;
+
+            this.urlBuilder.transparent = false;
         };
 
-        WorldWind.WmsLayer.call(this, cfg);
+        EoxSentinal2WithLabelsLayer.prototype = Object.create(EnhancedWmsLayer.prototype);
 
-        // Make this layer opaque
-        this.opacity = 1.0;
+        EoxSentinal2WithLabelsLayer.prototype.doRender = function (dc) {
+            WorldWind.WmsLayer.prototype.doRender.call(this, dc);
+            if (this.inCurrentFrame) {
+                dc.screenCreditController.addStringCredit("OpenStreetMap and Sentinel-2 cloudless by EOX IT Services GmbH", WorldWind.Color.LIGHT_GRAY);
+            }
+        };
 
-        this.urlBuilder.transparent = false;
-    };
-
-    EoxSentinal2WithLabelsLayer.prototype = Object.create(WorldWind.WmsLayer.prototype);
-
-    EoxSentinal2WithLabelsLayer.prototype.doRender = function (dc) {
-        WorldWind.WmsLayer.prototype.doRender.call(this, dc);
-        if (this.inCurrentFrame) {
-            dc.screenCreditController.addStringCredit("OpenStreetMap and Sentinel-2 cloudless by EOX IT Services GmbH", WorldWind.Color.DARK_GRAY);
-        }
-    };
-
-    return EoxSentinal2WithLabelsLayer;
-}
+        return EoxSentinal2WithLabelsLayer;
+    }
 );
