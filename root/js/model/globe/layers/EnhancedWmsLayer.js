dab0475b1539c179e89ac5e420911cd442f3a535
Bruce Schubert <bruce@emxsys.com>
2018-03-08 08:53:45 -0800

    Changed EnhancedWmsLayer to increase the size of its tile cache and fixed level set. - Original cache size was 500K. Increased to 4MB. - Changed the level zero delta to defacto standard for web mapping.

--- root/js/model/globe/layers/EnhancedWmsLayer.js
+++ root/js/model/globe/layers/EnhancedWmsLayer.js
@@ -14,46 +14,51 @@
  * @author Bruce Schubert
  */
 define(['worldwind'],
-        function (ww) {
-            "use strict";
+    function () {
+        "use strict";
 
-            var EnhancedWmsLayer = function (config, timeString) {
-                WorldWind.WmsLayer.call(this, config, timeString);
+        var EnhancedWmsLayer = function (config, timeString) {
+            WorldWind.WmsLayer.call(this, config, timeString);
 
-                // Extract the bbox out of the WMS layer configuration
-                this.bbox = config.sector;
+            // Extract the bbox out of the WMS layer configuration
+            this.bbox = config.sector;
 
-                // Override the default WmsLayer 36x36 level set with one that
-                // matches the GeoServer EPSG:4326 Gridset
-                this.levels = new WorldWind.LevelSet(WorldWind.Sector.FULL_SPHERE,
-                        new WorldWind.Location(90, 90),
-                        22,
-                        256,
-                        256);
+            // Override the default WmsLayer 36x36 level set with one that
+            // matches the GeoServer EPSG:4326 Gridset
+            this.levels = new WorldWind.LevelSet(WorldWind.Sector.FULL_SPHERE,
+                new WorldWind.Location(180, 180),
+                22,
+                256,
+                256);
 
-                // "tiled=true" is a hint for the GeoServer WMS to use the GeoWebCache
-                this.vendorParms = '&tiled=true&tilesorigin=-180,-90';
+            // "tiled=true" is a hint for the GeoServer WMS to use the GeoWebCache
+            this.vendorParms = '&tiled=true&tilesorigin=-180,-90';
 
-            };
+            // The WW tileCache is too small to accomodate large screen 
+            // full of tiles at an oblique view from the surface.
+            // Increase the size to prevent trashing of the tileCache.
+            this.tileCache = new WorldWind.MemoryCache(4.2e6, 3.2e6);
 
-            // Inherit the WmsLayer methods
-            EnhancedWmsLayer.prototype = Object.create(WorldWind.WmsLayer.prototype);
+        };
 
-            /**
-             * Returns the URL string for the resource. Overrides the TiledImageLayer
-             * resourceUrlForTile method by appending the vendor paramerters to the URL.
-             * @param {ImageTile} tile The tile whose image is returned
-             * @param {String} imageFormat The mime type of the image format desired.
-             * @returns {String} The URL string, or null if the string can not be formed.
-             */
-            EnhancedWmsLayer.prototype.resourceUrlForTile = function (tile, imageFormat) {
-                var url = WorldWind.TiledImageLayer.prototype.resourceUrlForTile.call(this, tile, imageFormat);
-                if (url) {
-                    url = url + this.vendorParms;
-                }
-                return url;
-            };
+        // Inherit the WmsLayer methods
+        EnhancedWmsLayer.prototype = Object.create(WorldWind.WmsLayer.prototype);
 
-            return EnhancedWmsLayer;
-        }
+        /**
+         * Returns the URL string for the resource. Overrides the TiledImageLayer
+         * resourceUrlForTile method by appending the vendor paramerters to the URL.
+         * @param {ImageTile} tile The tile whose image is returned
+         * @param {String} imageFormat The mime type of the image format desired.
+         * @returns {String} The URL string, or null if the string can not be formed.
+         */
+        EnhancedWmsLayer.prototype.resourceUrlForTile = function (tile, imageFormat) {
+            var url = WorldWind.TiledImageLayer.prototype.resourceUrlForTile.call(this, tile, imageFormat);
+            if (url) {
+                url = url + this.vendorParms;
+            }
+            return url;
+        };
+
+        return EnhancedWmsLayer;
+    }
 );
