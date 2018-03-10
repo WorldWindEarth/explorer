6ddd6d1827545a7ee645f303e7a4c9e82f576ef2
Bruce Schubert <bruce@emxsys.com>
2018-03-10 14:04:10 -0800

    Adjusted the resolution of the UsgsImageryTopoBaseMapLayer.

--- root/js/model/globe/layers/UsgsImageryTopoBaseMapLayer.js
+++ root/js/model/globe/layers/UsgsImageryTopoBaseMapLayer.js
@@ -18,40 +18,40 @@ define([
     'model/globe/layers/EnhancedWmsLayer',
     'worldwind'],
     function (EnhancedWmsLayer) {
-    "use strict";
+        "use strict";
 
-    /**
-     * Constructs a USGS Imagery Topo map layer.
-     * @constructor
-     * @augments WmsLayer
-     */
-    var UsgsImageryTopoBaseMapLayer = function () {
-        var cfg = {
-            title: "USGS Imagery Topo Basemap",
-            version: "1.3.0",
-            service: "https://basemap.nationalmap.gov:443/arcgis/services/USGSImageryTopo/MapServer/WmsServer?",
-            layerNames: "0",
-            sector: new WorldWind.Sector(-90.0, 90.0, -180, 180),
-            levelZeroDelta: new WorldWind.Location(180,180),
-            numLevels: 12,
-            format: "image/png",
-            size: 256,
-            coordinateSystem: "EPSG:4326", // optional
-            styleNames: "" // (optional): {String} A comma separated list of the styles to include in this layer.</li>
+        /**
+         * Constructs a USGS Imagery Topo map layer.
+         * @constructor
+         * @augments WmsLayer
+         */
+        var UsgsImageryTopoBaseMapLayer = function () {
+            var cfg = {
+                title: "USGS Imagery Topo Basemap",
+                version: "1.3.0",
+                service: "https://basemap.nationalmap.gov:443/arcgis/services/USGSImageryTopo/MapServer/WmsServer?",
+                layerNames: "0",
+                sector: new WorldWind.Sector(-90.0, 90.0, -180, 180),
+                levelZeroDelta: new WorldWind.Location(180, 180),
+                numLevels: 15,
+                format: "image/png",
+                size: 256,
+                coordinateSystem: "EPSG:4326", // optional
+                styleNames: "" // (optional): {String} A comma separated list of the styles to include in this layer.</li>
+            };
+
+            EnhancedWmsLayer.call(this, cfg);
+
+            // Make this layer opaque
+            this.opacity = 1.0;
+
+            // Requesting tiles with transparency (the nominal case) causes the layer's labels to bleed 
+            // the underlying background layer which makes for a rather ugly map.
+            this.urlBuilder.transparent = false;
         };
 
-        EnhancedWmsLayer.call(this, cfg);
+        UsgsImageryTopoBaseMapLayer.prototype = Object.create(EnhancedWmsLayer.prototype);
 
-        // Make this layer opaque
-        this.opacity = 1.0;
-
-        // Requesting tiles with transparency (the nominal case) causes the layer's labels to bleed 
-        // the underlying background layer which makes for a rather ugly map.
-        this.urlBuilder.transparent = false;
-    };
-
-    UsgsImageryTopoBaseMapLayer.prototype = Object.create(EnhancedWmsLayer.prototype);
-
-    return UsgsImageryTopoBaseMapLayer;
-}
+        return UsgsImageryTopoBaseMapLayer;
+    }
 );
