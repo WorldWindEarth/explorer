fe706b0911f1efb822a81bb9839d460d2b5006c6
Bruce Schubert <bruce@emxsys.com>
2018-03-10 12:24:26 -0800

    Fix URL builders for WMS, WCC and Row/Column layers. - Fixes #111.

--- root/js/WorldWindFixes.js
+++ root/js/WorldWindFixes.js
@@ -312,7 +312,146 @@ define(['worldwind'], function () {
 
                 return texture;
             };
+            
+            
+            /**
+             * WmsUrlBuilder.urlForTile
+             * 
+             * Fixes replace " " with %20 with a global replace.
+             */
+            WorldWind.WmsUrlBuilder.prototype.urlForTile = function (tile, imageFormat) {
+                if (!tile) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WmsUrlBuilder", "urlForTile", "missingTile"));
+                }
 
+                if (!imageFormat) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WmsUrlBuilder", "urlForTile",
+                            "The image format is null or undefined."));
+                }
+
+                var sector = tile.sector;
+
+                var sb = WorldWind.WmsUrlBuilder.fixGetMapString(this.serviceAddress);
+
+                if (sb.search(/service=wms/i) < 0) {
+                    sb = sb + "service=WMS";
+                }
+
+                sb = sb + "&request=GetMap";
+                sb = sb + "&version=" + this.wmsVersion;
+                sb = sb + "&transparent=" + (this.transparent ? "TRUE" : "FALSE");
+                sb = sb + "&layers=" + this.layerNames;
+                sb = sb + "&styles=" + this.styleNames;
+                sb = sb + "&format=" + imageFormat;
+                sb = sb + "&width=" + tile.tileWidth;
+                sb = sb + "&height=" + tile.tileHeight;
+
+                if (this.timeString) {
+                    sb = sb + "&time=" + this.timeString;
+                }
+
+                if (this.isWms130OrGreater) {
+                    sb = sb + "&crs=" + this.crs;
+                    sb = sb + "&bbox=";
+                    if (this.crs === "CRS:84") {
+                        sb = sb + sector.minLongitude + "," + sector.minLatitude + ",";
+                        sb = sb + sector.maxLongitude + "," + sector.maxLatitude;
+                    } else {
+                        sb = sb + sector.minLatitude + "," + sector.minLongitude + ",";
+                        sb = sb + sector.maxLatitude + "," + sector.maxLongitude;
+                    }
+                } else {
+                    sb = sb + "&srs=" + this.crs;
+                    sb = sb + "&bbox=";
+                    sb = sb + sector.minLongitude + "," + sector.minLatitude + ",";
+                    sb = sb + sector.maxLongitude + "," + sector.maxLatitude;
+                }
+
+                // Global replace space
+                sb = sb.replace(/\s/g, "%20");
+
+                return sb;
+            };
+
+
+            /**
+             * WcsTileUrlBuilder.urlForTile
+             * 
+             * Fixes replace " " with %20 with a global replace.
+             */
+            WorldWind.WcsTileUrlBuilder.prototype.urlForTile = function (tile, coverageFormat) {
+
+                if (!tile) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WcsUrlBuilder", "urlForTile", "missingTile"));
+                }
+
+                if (!coverageFormat) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WcsUrlBuilder", "urlForTile",
+                            "The coverage format is null or undefined."));
+                }
+
+                var sector = tile.sector;
+
+                var sb = WorldWind.WcsTileUrlBuilder.fixGetCoverageString(this.serviceAddress);
+
+                if (sb.search(/service=wcs/i) < 0) {
+                    sb = sb + "service=WCS";
+                }
+
+                sb = sb + "&request=GetCoverage";
+                sb = sb + "&version=" + this.wcsVersion;
+                sb = sb + "&coverage=" + this.coverageName;
+                sb = sb + "&format=" + coverageFormat;
+                sb = sb + "&width=" + tile.tileWidth;
+                sb = sb + "&height=" + tile.tileHeight;
+
+                sb = sb + "&crs=" + this.crs;
+                sb = sb + "&bbox=";
+                sb = sb + sector.minLongitude + "," + sector.minLatitude + ",";
+                sb = sb + sector.maxLongitude + "," + sector.maxLatitude;
+
+                // Global replace space
+                sb = sb.replace(/\s/g, "%20");
+
+                return sb;
+            };
+
+            /**
+             * LevelRowColumnUrlBuilder.urlForTile
+             * 
+             * Fixes replace " " with %20 with a global replace.
+             */
+            WorldWind.LevelRowColumnUrlBuilder.prototype.urlForTile = function (tile, imageFormat) {
+                if (!tile) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WmsUrlBuilder", "urlForTile", "missingTile"));
+                }
+
+                if (!imageFormat) {
+                    throw new ArgumentError(
+                        WorldWind.Logger.logMessage(WorldWind.Logger.LEVEL_SEVERE, "WmsUrlBuilder", "urlForTile",
+                            "The image format is null or undefined."));
+                }
+
+                var sb = this.serverAddress;
+
+                if (this.pathToData) {
+                    sb = sb + "/" + this.pathToData;
+                }
+
+                sb = sb + "/" + tile.level.levelNumber.toString();
+                sb = sb + "/" + tile.row.toString();
+                sb = sb + "/" + tile.row.toString() + "_" + tile.column.toString();
+                sb = sb + "." + WorldWind.WWUtil.suffixForMimeType(imageFormat);
+
+                // Global replace space
+                sb = sb.replace(/\s/g, "%20");
+                return sb;
+            };
 
         } // end if 0.9.0
     };
